d170681ff420a139ea35aeb3d44ad4fbd13bb8b5
Niklas Loeser <niklas@4loeser.net>
2020-04-24 23:28:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# conf
+
+TODO: Write a description here
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     conf:
+       github: your-github-user/conf
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "conf"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/conf/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Niklas Loeser](https://github.com/your-github-user) - creator and maintainer
---
+++ src/conf.cr
@@ -0,0 +1,106 @@
+require "toml"
+
+class Conf
+
+  @table = uninitialized TOML::Table
+
+  def initialize(@file : String | Path)
+    reload
+  end
+
+  def reload
+    @table = TOML.parse_file @file
+  end
+
+  def []=(key : String, value : TOML::Type)
+    @table[key] = value
+  end
+
+  def [](key : String)
+    @table[key]
+  end
+
+  def as_s(key : String)
+    @table[key].as(String)
+  end
+
+  def as_b(key : String)
+    @table[key].as(Bool)
+  end
+
+  def as_i(key : String)
+    @table[key].as(Int64)
+  end
+
+  def as_f(key : String)
+    @table[key].as(Float64)
+  end
+
+  def as_t(key : String)
+    @table[key].as(Time)
+  end
+
+  def as_a(key : String)
+    @table[key].as(Array(TOML::Type))
+  end
+
+  def as_h(key : String)
+    @table[key].as(TOML::Table)
+  end
+
+  def as_s?(key : String, default : String = "")
+    if @table[key].is_a?(String)
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_b?(key : String, default : Bool = false)
+    if @table[key].is_a?(Bool)
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_i?(key : String, default : Int64 = 0)
+    if @table[key].is_a?(Int64)
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_f?(key : String, default : Float64 = 0)
+    if @table[key].is_a?(Float64)
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_t?(key : String, default : Time)
+    if @table[key].is_a?(Time)
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_a?(key : String, default : Array(TOML::Type) = [] of Array(TOML::Type))
+    if @table[key].is_a?(Array(TOML::Type))
+      @table[key]
+    else
+      default
+    end
+  end
+
+  def as_h?(key : String, default : TOML::Table)
+    if @table[key].is_a?(TOML::Table)
+      @table[key]
+    else
+      default
+    end
+  end
+end
